16a5401dbf4bcbc282619992eac9577e97a69c24
Saurabh Kumar <kumarsaurabh08112003@gmail.com>
2025-04-20 17:05:35 +0530

    Bottom Banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
 import BestSellers from '../components/BestSellers'
+import BottomBanner from '../components/BottomBanner'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <MainBanner/>
       <Categories/>
       <BestSellers/>
+      <BottomBanner/>
     </div>
   )
 }
