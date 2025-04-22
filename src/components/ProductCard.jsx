c49a7749d7c1364f0b31751abb48bcb34c1ddf17
Saurabh Kumar <kumarsaurabh08112003@gmail.com>
2025-04-22 23:29:22 +0530

    product card updated

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { assets } from "../assets/assets";
 import { useAppContext } from "../context/AppContext";
 
+
 const ProductCard = ({product}) => {
 
-    const {currency , cartItems  , addToCart , removeCartItem , updateCartItem} = useAppContext()
+    const {currency , cartItems  , addToCart , removeCartItem , updateCartItem , navigate} = useAppContext()
 
     return product && (
         <div className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
             <div className="group cursor-pointer flex items-center justify-center px-2">
-                <img className="group-hover:scale-105 transition max-w-26 md:max-w-36" src={product.image[0]} alt={product.name} />
+                <img onClick={()=> { navigate(`/products/${product.category.toLowerCase()}/${product._id}` )
+                ; scrollTo(0,0)}
+                 } 
+                 className="group-hover:scale-105 transition max-w-26 md:max-w-36" src={product.image[0]} alt={product.name} />
             </div>
             <div className="text-gray-500/60 text-sm">
                 <p>{product.category}</p>
