84f3c6715acc8069532f09f4b415373220588e66
Saurabh <kumarsaurabh08112003@gmail.com>
2025-08-02 17:10:55 +0530

    mobile view improved

--- client/src/components/BestSellers.jsx
+++ client/src/components/BestSellers.jsx
@@ -9,7 +9,7 @@ const BestSeller = () => {
   return (
     <div className='mt-16' id='best-seller'>
        <p className='text-2xl md:text-3xl font-medium mb-8'>Best Seller</p>
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-6 mb-16'>
+  <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-3 md:gap-6 mb-16'>
           {products.filter((product) => product.inStock).slice(0,5).map((product, index)=>(
             <ProductCard key={index} product={product} />
           ))}
